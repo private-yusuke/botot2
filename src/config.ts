4efd290c58bc26e072e75f997e063a10c2f7fee1
sobadon <sbdn@chasoba.net>
2020-09-14 19:51:32 +0000

    Backward compatibility

--- src/config.ts
+++ src/config.ts
@@ -130,4 +130,7 @@ config.visibility = getProperVisibilityProperty(config)
 if (config.database.attenuationRate == undefined)
 	config.database.attenuationRate = 0
 
+if (config.markov.allowLearnVisFollowers == undefined)
+	config.markov.allowLearnVisFollowers = true
+
 export default config as Config

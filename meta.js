ca451e9e0e6c7389b3a4289dfb6cb93f637181ed
takonomura <takonomura@users.noreply.github.com>
2020-09-27 02:28:41 +0900

    Run prettier

--- meta.js
+++ meta.js
@@ -2,6 +2,12 @@ const fs = require("fs")
 const { execFileSync } = require("child_process")
 const meta = require("./package.json")
 
-let gitRev = execFileSync("git", ["rev-parse", "--short", "HEAD"]).toString().trim()
+let gitRev = execFileSync("git", ["rev-parse", "--short", "HEAD"])
+	.toString()
+	.trim()
 
-fs.writeFileSync("./built/meta.json", JSON.stringify({ version: meta.version, revision: gitRev }), "utf-8")
+fs.writeFileSync(
+	"./built/meta.json",
+	JSON.stringify({ version: meta.version, revision: gitRev }),
+	"utf-8"
+)

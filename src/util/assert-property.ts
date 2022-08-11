5750143c5cb67df85eeb6c2d361f5dd3c159813e
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2022-08-11 18:12:35 +0900

    Add typedoc

--- src/util/assert-property.ts
+++ src/util/assert-property.ts
@@ -1,5 +1,11 @@
 export type PickRequired<T, K extends keyof T> = T & Required<Pick<T, K>>
 
+/**
+ * Type argument K must be a literal type representing exactly one key given to `key`.
+ * Normally, the type arguments for this function should be left to type inference rather than being typed in manually.
+ * @param key - a key of property in type T
+ * @returns A assertion function that checks if a value of type T has the property of `key`
+ */
 export function assertProperty<T, K extends keyof T>(
 	key: K
 ): (value: T) => value is PickRequired<T, K> {
